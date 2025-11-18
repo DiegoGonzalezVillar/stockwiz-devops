a85b75cb0f39208aec35f17d1a9df0eb34155148
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2025-11-18 10:03:48 -0300

    feat(ci): add carpeta infra a develop

---
+++ infra/variables.tf
@@ -0,0 +1,32 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment (dev, test, prod)"
+  type        = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_profile" {
+  type = string
+}
+
+variable "ecr_repositories" {
+  type    = list(string)
+  default = ["api-gateway", "product-service", "inventory-service"]
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
+
+
+
+
