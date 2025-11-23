b66e1bc632637734689e39621fbfc410d29c850e
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2025-11-23 13:04:12 -0300

    feat(terraform): add terraform base structure

--- infra/modules/alb_internal/outputs.tf
+++ infra/modules/alb_internal/outputs.tf
@@ -1,19 +1,11 @@
 output "alb_dns_name" {
-  description = "DNS interno del ALB"
-  value       = aws_lb.this.dns_name
+  value = aws_lb.internal.dns_name
 }
 
-output "alb_arn" {
-  description = "ARN del ALB interno"
-  value       = aws_lb.this.arn
+output "product_tg_arn" {
+  value = aws_lb_target_group.product.arn
 }
 
-output "target_group_arn" {
-  description = "ARN del target group interno"
-  value       = aws_lb_target_group.this.arn
-}
-
-output "listener_arn" {
-  description = "ARN del listener interno"
-  value       = aws_lb_listener.this.arn
+output "inventory_tg_arn" {
+  value = aws_lb_target_group.inventory.arn
 }
