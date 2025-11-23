06296550acddde0467e3446d923fd7a12c231417
Diego Gonzalez Villar <43786303+DiegoGonzalezVillar@users.noreply.github.com>
2025-11-22 21:46:25 -0300

    Update variables.tf

--- infra/modules/ecs_services/variables.tf
+++ infra/modules/ecs_services/variables.tf
@@ -3,6 +3,10 @@ variable "cluster_name" { type = string }
 variable "public_subnets_ids" { type = list(string) }
 variable "ecs_sg_id" { type = string }
 variable "gateway_tg_arn" { type = string }
+variable "vpc_id" {
+  type = string
+}
+
 
 variable "gateway_image" { type = string }
 variable "product_image" { type = string }
