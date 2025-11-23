1c8fe2415d1794b5cafcbd7a114a758590b121f2
Diego Gonzalez Villar <43786303+DiegoGonzalezVillar@users.noreply.github.com>
2025-11-22 21:44:57 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -33,15 +33,16 @@ resource "aws_ecs_cluster" "fargate" {
 }
 
 module "ecs_services" {
-  source             = "./modules/ecs_services"
-  environment        = var.env
-  cluster_name       = "ecs-fargate-cluster-${var.env}"
-  public_subnets_ids = module.vpc.public_subnets_ids
-  ecs_sg_id          = module.vpc.ecs_sg_id
-  gateway_tg_arn     = module.alb.gateway_tg_arn
-
-  gateway_image   = "${module.ecr.repo_uris["api-gateway"]}:${var.env}-latest"
-  product_image   = "${module.ecr.repo_uris["product-service"]}:${var.env}-latest"
-  inventory_image = "${module.ecr.repo_uris["inventory-service"]}:${var.env}-latest"
+  source              = "./modules/ecs_services"
+  environment         = var.env
+  cluster_name        = "ecs-fargate-cluster-${var.env}"
+  public_subnets_ids  = module.vpc.public_subnets_ids
+  private_subnets_ids = module.vpc.private_subnets_ids
+  ecs_sg_id           = module.vpc.ecs_sg_id
+  gateway_tg_arn      = module.alb.gateway_tg_arn
+  vpc_id              = module.vpc.vpc_id
+  gateway_image       = "${module.ecr.repo_uris["api-gateway"]}:${var.env}-latest"
+  product_image       = "${module.ecr.repo_uris["product-service"]}:${var.env}-latest"
+  inventory_image     = "${module.ecr.repo_uris["inventory-service"]}:${var.env}-latest"
 }
 
