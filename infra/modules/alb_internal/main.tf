32e625d3cac5824e6bda7eb5659260b4cc04b732
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2025-11-23 18:44:37 -0300

    feat(terraform): add terraform base structure

---
+++ infra/modules/alb_internal/main.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "this" {
+  name               = "${var.name}-alb-internal"
+  internal           = true
+  load_balancer_type = "application"
+
+  security_groups = [var.sg_id]
+  subnets         = var.private_subnets
+
+  tags = {
+    Name        = "${var.name}-alb-internal"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "service" {
+  name        = "${var.name}-tg"
+  port        = var.port
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    path                = "/health"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 20
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name        = "${var.name}-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = var.port
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.service.arn
+  }
+}
