9a4a5fb1df2bf98bd9d520ac5981484609483ec7
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2025-11-23 19:20:46 -0300

    feat(terraform): add terraform base structure

--- infra/modules/ecs_services/main.tf
+++ infra/modules/ecs_services/main.tf
@@ -67,7 +67,7 @@ resource "aws_ecs_service" "dbcache" {
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = var.private_subnets_ids
+    subnets          = [var.private_subnets_ids[0]]
     security_groups  = [var.ecs_sg_id]
     assign_public_ip = false
   }
@@ -180,7 +180,7 @@ resource "aws_ecs_service" "product" {
   desired_count   = 1
 
   network_configuration {
-    subnets          = var.private_subnets_ids
+    subnets          = [var.private_subnets_ids[0]]
     security_groups  = [var.ecs_sg_id]
     assign_public_ip = false
   }
@@ -239,7 +239,7 @@ resource "aws_ecs_service" "inventory" {
   desired_count   = 1
 
   network_configuration {
-    subnets          = var.private_subnets_ids
+    subnets          = [var.private_subnets_ids[0]]
     security_groups  = [var.ecs_sg_id]
     assign_public_ip = false
   }
