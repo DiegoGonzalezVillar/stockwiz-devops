3aa52bc951be8b479bbc62142cfd3ed4a99004d4
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2025-11-23 14:52:15 -0300

    feat(terraform): add terraform base structure

--- infra/modules/ecs_services/main.tf
+++ infra/modules/ecs_services/main.tf
@@ -102,10 +102,11 @@ resource "aws_ecs_task_definition" "gateway" {
       }]
 
       environment = [
-        { name = "PRODUCT_SERVICE_URL",  value = var.dns_product },
-        { name = "INVENTORY_SERVICE_URL", value = var.dns_inventory },
-        { name = "REDIS_URL", value = "redis://dbcache:6379" }
-      ]
+  { name = "PRODUCT_SERVICE_URL",  value = "http://${var.dns_product}:8001" },
+  { name = "INVENTORY_SERVICE_URL", value = "http://${var.dns_inventory}:8002" },
+  { name = "REDIS_URL", value = "redis://${var.dns_dbcache}:6379" }
+]
+
 
       logConfiguration = {
         logDriver = "awslogs"
@@ -162,10 +163,11 @@ resource "aws_ecs_task_definition" "product" {
         containerPort = 8001
       }]
 
-      environment = [
-        { name = "DATABASE_URL", value = "postgresql://admin:admin123@dbcache:5432/microservices_db" },
-        { name = "REDIS_URL",     value = "redis://dbcache:6379" }
-      ]
+     environment = [
+  { name = "DATABASE_URL", value = "postgresql://admin:admin123@${var.dns_dbcache}:5432/microservices_db" },
+  { name = "REDIS_URL",     value = "redis://${var.dns_dbcache}:6379" }
+]
+
 
       logConfiguration = {
         logDriver = "awslogs"
@@ -223,9 +225,10 @@ resource "aws_ecs_task_definition" "inventory" {
       }]
 
       environment = [
-        { name = "DATABASE_URL", value = "postgresql://admin:admin123@dbcache:5432/microservices_db" },
-        { name = "REDIS_URL",     value = "redis://dbcache:6379" }
-      ]
+  { name = "DATABASE_URL", value = "postgresql://admin:admin123@${var.dns_dbcache}:5432/microservices_db" },
+  { name = "REDIS_URL",     value = "redis://${var.dns_dbcache}:6379" }
+]
+
 
       logConfiguration = {
         logDriver = "awslogs"
