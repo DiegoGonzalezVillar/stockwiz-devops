17dc10980bd5de9ea2cd78506594203823975f9f
Diego Gonzalez Villar <43786303+DiegoGonzalezVillar@users.noreply.github.com>
2025-11-24 16:52:43 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,37 +17,35 @@ sleep 2
 ############################################
 echo "[2/5] Starting PostgreSQL..."
 service postgresql start
-sleep 2
+sleep 3
 
 echo "Loading initial SQL..."
 sudo -u postgres psql -f /app/init.sql || true
-echo "SQL loaded."
 
 ############################################
 # 3) Start API Gateway
 ############################################
 echo "[3/5] Starting API Gateway..."
-/app/api-gateway/api-gateway > /app/api-gateway.log 2>&1 &
+/app/api-gateway > /app/api.log 2>&1 &
 
 ############################################
-# 4) Start Product-Service
+# 4) Start Product Service
 ############################################
 echo "[4/5] Starting Product Service..."
 python3 /app/product-service/main.py > /app/product.log 2>&1 &
 
 ############################################
-# 5) Start Inventory-Service
+# 5) Start Inventory Service
 ############################################
 echo "[5/5] Starting Inventory Service..."
-/app/inventory-service/inventory-service > /app/inventory.log 2>&1 &
+/app/inventory-service > /app/inventory.log 2>&1 &
 
 ############################################
 # 6) Logs + wait
 ############################################
-echo ""
 echo "===================================="
 echo " ALL SERVICES STARTED SUCCESSFULLY"
 echo "===================================="
-echo "Tailing logs..."
-
 tail -f /app/*.log
+
+
