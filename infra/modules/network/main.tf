7f73a1cfee5e0a86fdd6d90cb6ccac6649f5c6eb
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2025-11-23 21:48:58 -0300

    feat(terraform): add terraform base structure

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -5,18 +5,46 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
 
-# Subnet 1 (AZ a)
+# Internet Gateway (Necesario para ALB + ECS con IP pública)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Subnet 1
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, 1)
   availability_zone = "${var.aws_region}a"
+  map_public_ip_on_launch = true
 }
 
-# Subnet 2 (AZ b)
+# Subnet 2
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, 2)
   availability_zone = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+}
+
+# Associate subnets to the public route table
+resource "aws_route_table_association" "public_1_assoc" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2_assoc" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_security_group" "ecs_sg" {
@@ -54,3 +82,5 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
