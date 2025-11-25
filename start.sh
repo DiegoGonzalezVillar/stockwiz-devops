431faa6e0326771cb26cf6e7f896796d18596552
Diego Gonzalez Villar <43786303+DiegoGonzalezVillar@users.noreply.github.com>
2025-11-25 00:36:14 -0300

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "===================================="
-echo " STARTING FULLSTACK CONTAINER"
-echo "===================================="
-
-echo "Starting supervisord..."
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
