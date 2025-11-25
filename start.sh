ea91bb7dba40ca1f120c6c902cfc4d090a7d7e9e
Diego Gonzalez Villar <43786303+DiegoGonzalezVillar@users.noreply.github.com>
2025-11-25 10:34:54 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,21 +7,26 @@ echo "===================================="
 
 mkdir -p /app/logs
 
-# Inicializar PostgreSQL si no existe
+PG_BIN=/usr/lib/postgresql/14/bin
+
 if [ ! -f "/var/lib/postgresql/data/PG_VERSION" ]; then
     echo "Initializing PostgreSQL data directory..."
-    su postgres -c "/usr/lib/postgresql/14/bin/initdb -D /var/lib/postgresql/data"
+    su postgres -c "$PG_BIN/initdb -D /var/lib/postgresql/data"
 
-    echo "Creating user, password and database..."
-    su postgres -c "pg_ctl -D /var/lib/postgresql/data -l /app/logs/postgres-init.log start"
-    sleep 3
+    echo "Starting temporary Postgres..."
+    su postgres -c "$PG_BIN/pg_ctl -D /var/lib/postgresql/data -l /app/logs/postgres-init.log start"
+    sleep 4
 
+    echo "Creating DB + User..."
     su postgres -c "psql -c \"CREATE USER admin WITH PASSWORD 'admin123';\""
     su postgres -c "psql -c \"CREATE DATABASE microservices_db OWNER admin;\""
     su postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE microservices_db TO admin;\""
 
-    su postgres -c "pg_ctl -D /var/lib/postgresql/data stop"
+    echo "Stopping temporary Postgres..."
+    su postgres -c "$PG_BIN/pg_ctl -D /var/lib/postgresql/data stop"
 fi
 
 echo "Starting supervisord..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
+
