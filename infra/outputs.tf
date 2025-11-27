9a7552297651511ac0cd8589bed942765a1a8502
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2025-11-27 01:18:08 -0300

    fix(infra): arreglo en root/outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,4 +6,8 @@ output "alb_dns" {
 #  value = aws_cloudwatch_dashboard.main.dashboard_name
 #}
 
+output "alb_dns_name" {
+  value       = aws_lb.app_lb.dns_name 
+  description = "The DNS name of the Application Load Balancer for accessing the API Gateway."
+}
 
